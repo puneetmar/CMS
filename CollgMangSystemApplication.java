fb8db3e16918ed042461e9b534401f76b36c99cd
Puneet Marwah <137035091+puneetmar@users.noreply.github.com>
2023-10-05 00:30:06 +0530

    Add files via upload

---
+++ CollgMangSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cms.collgmangsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollgMangSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollgMangSystemApplication.class, args);
+	}
+
+}
